6abfbdb9089302a09ab05b036d24b8441fa5a0cb
Nathan Levitt <nathanlevitt@Nathans-MacBook-Pro-5.local>
2016-09-17 10:37:32 -0700

    Initial Commit

---
+++ TAP - Throw A Party/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TAP - Throw A Party
+//
+//  Created by Nathan Levitt on 9/17/16.
+//  Copyright © 2016 The Bench Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
